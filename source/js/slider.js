7bc470df5db1ecb1dc0961f7c6450cc001bca456
sigmarius <aamokeeva@yandex.ru>
2021-01-21 23:00:02 +0300

    Создает разметку страницы Каталог

--- source/js/slider.js
+++ source/js/slider.js
@@ -4,4 +4,25 @@
 
 (function () {
 
+  // var list = document.querySelector('.slider__list');
+  // var items = document.querySelectorAll('.slider__item');
+  // var buttonLeft = document.querySelector('.slider__button--left');
+  // var buttonRight = document.querySelector('.slider__button--right');
+
+  // var wrapperWidth = parseFloat(getComputedStyle(list).width);
+  // var itemWidth = parseFloat(getComputedStyle(items[0]));
+  // var positionLeftItem = 0;
+  // var transform = 0;
+  // var step = wrapperWidth;
+  // var itemsArray = [];
+
+  // items.forEach(function (item, index) {
+  //   itemsArray.push({item: item, position: index, transform: 0});
+  // });
+
+  // var position = {
+  //   getMin: 0,
+  //   getMax: itemsArray.length - 1,
+  // };
+
 })();
