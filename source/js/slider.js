eabd0ec95ebb08ea42fb2cc1dac7dc4eabbe4dea
sigmarius <aamokeeva@yandex.ru>
2021-02-01 23:10:46 +0300

    Начинает реализовывать логику слайдера

--- source/js/slider.js
+++ source/js/slider.js
@@ -4,6 +4,13 @@
 
 (function () {
 
+  // var PictureCount = {
+  //   DESKTOP: 4,
+  //   TABLET: 2,
+  // };
+
+  var tablet = window.matchMedia('(max-width: 1023px)');
+
   var list = document.querySelector('.slider__list');
   var items = document.querySelectorAll('.slider__item');
   var buttonLeft = document.querySelector('.slider__button--left');
@@ -12,36 +19,71 @@
   var wrapperWidth = parseFloat(getComputedStyle(list).width);
   var itemWidth = parseFloat(getComputedStyle(items[0]).width);
 
-  var position = 0;
+  var positionLeftItem = 0;
+  var transform = 0;
+  // var step = itemWidth / wrapperWidth * 100 * 4;
+  var step = itemWidth / wrapperWidth * 100;
+  var itemsArray = [];
 
-  console.log(wrapperWidth);
-  console.log(itemWidth);
-
-  // var positionLeftItem = 0;
-  // var transform = 0;
-  // var step = wrapperWidth;
-  // var itemsArray = [];
-
-  // items.forEach(function (item, index) {
-  //   itemsArray.push({item: item, position: index, transform: 0});
-  // });
-
-  // var position = {
-  //   getMin: 0,
-  //   getMax: itemsArray.length - 1,
-  // };
-
-  buttonLeft.addEventListener('click', function () {
-    position += wrapperWidth;
-    position = Math.min(position, 0);
-    console.log(list.style.transform);
-    list.style.transform = 'translateX(' + position + '%)';
+  items.forEach(function (item, index) {
+    itemsArray.push({item: item, position: index, transform: 0});
   });
 
+  var position = {
+    getMin: 0,
+    getMax: itemsArray.length,
+  };
+
+  var count = 4; // временная переменная для определения количества изображений на адаптиве
+  // var step;
+
+  // var changeSizeHandler = function (evt) {
+  //   if (evt.matches) {
+  //     count = PictureCount.TABLET;
+  //     console.log(count);
+  //   } else {
+  //     count = PictureCount.DESKTOP;
+  //     console.log(count);
+  //   }
+  //   // step = itemWidth / wrapperWidth * 100 * count;
+  // };
+
+  // tablet.addListener(changeSizeHandler);
+  // changeSizeHandler(tablet);
+
   buttonRight.addEventListener('click', function () {
-    position -= wrapperWidth;
-    position = Math.max(position, -itemWidth * items.length - 4);
-    list.style.marginLeft = position + 'px';
-  })
+    if ((positionLeftItem + wrapperWidth / itemWidth + count - 1) >= position.getMax) {
+      return;
+    }
+
+    positionLeftItem++;
+    transform -= step * count;
+
+    list.style.transform = 'translateX(' + transform + '%)';
+  });
+
+  buttonLeft.addEventListener('click', function () {
+    if (positionLeftItem <= position.getMin) {
+      return;
+    }
+
+    positionLeftItem--;
+    transform += step * count;
+
+    list.style.transform = 'translateX(' + transform + '%)';
+  });
+
+  // buttonLeft.addEventListener('click', function () {
+  //   position += wrapperWidth;
+  //   position = Math.min(position, 0);
+  //   console.log(list.style.transform);
+  //   list.style.transform = 'translateX(' + position + '%)';
+  // });
+
+  // buttonRight.addEventListener('click', function () {
+  //   position -= wrapperWidth;
+  //   position = Math.max(position, -itemWidth * items.length - 4);
+  //   list.style.marginLeft = position + 'px';
+  // });
 
 })();
